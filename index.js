1adf697841c0d4e5dabb94ecfab0151f0427f271
Adam Duro <adam@duromedia.com>
2019-06-19 21:26:41 -0700

    Initial Plugin Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+class ServerlessBinaryCors {
+  constructor(serverless, options) {
+    this.serverless = serverless
+    this.options = options
+
+    this.hooks = {
+      'before:package:finalize': this.addBinaryResponseTemplate.bind(this),
+    }
+  }
+
+  addBinaryResponseTemplate() {
+    let Resources = this.serverless.service.provider.compiledCloudFormationTemplate.Resources
+    for (let key in Resources) {
+      if (this.isCorsMethod(Resources, key)) {
+        this.modifyOptionsMethod(Resources[key])
+      }
+    }
+  }
+
+  modifyOptionsMethod(methodCF) {
+    let IntegrationResponses = methodCF.Properties.Integration.IntegrationResponses
+    for (const index in IntegrationResponses) {
+      IntegrationResponses[index].ResponseTemplates = {
+        // 'application/json': '#set($origin = $input.params("Origin"))\n#if($origin == "") #set($origin = $input.params("origin")) #end\n#if($origin == "*") #set($context.responseOverride.header.Access-Control-Allow-Origin = $origin) #end',
+        'image/jpeg': '',
+      }
+    }
+  }
+
+  isCorsMethod(resources, key) {
+    return (
+      resources[key].Type === 'AWS::ApiGateway::Method' &&
+      resources[key].Properties.HttpMethod === 'OPTIONS' &&
+      resources[key].Properties.Integration.Type === 'MOCK'
+    )
+  }
+}
+
+module.exports = ServerlessBinaryCors
